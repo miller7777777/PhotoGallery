571d47591d09d765a6967349c104ebb81217b0cd
miller777 <miller777@mail.ru>
2018-05-27 03:29:17 +0300

    Broadcast receiver:

--- app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package com.bignerdranch.android.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.widget.Toast;
 
 
@@ -17,7 +19,7 @@ public abstract class VisibleFragment extends Fragment {
         super.onStart();
 
         IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
-        getActivity().registerReceiver(mOnShowNotification, filter);
+        getActivity().registerReceiver(mOnShowNotification, filter, PollService.PERM_PRIVATE, null);
     }
 
     @Override
@@ -30,9 +32,10 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast: " + intent.getAction(),
-                    Toast.LENGTH_LONG).show();
+            // Получение означает, что пользователь видит приложение,
+            // поэтому оповещение отменяется
+            Log.i(TAG, "canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }
